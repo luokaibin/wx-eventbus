34e91ecff23b544d79bdb1bbe3788f836715310b
luokaibin <15686123825@163.com>
2020-05-08 23:00:29 +0800

    更新策略模式demo

---
+++ components/icon-menu-item/index.js
@@ -0,0 +1,13 @@
+Component({
+  properties: {
+    menu: Object
+  },
+
+  data: {
+
+  },
+
+  methods: {
+
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,50 @@
 Component({
 
   data: {
-
+    iconMenu: [
+      {
+        text: '奖品',
+        icon: 'icon-libao',
+        color: '#e94466',
+      },
+      {
+        text: '礼包',
+        icon: 'icon-gouwu',
+        color: '#df0614'
+      },
+      {
+        text: '评论',
+        icon: 'icon-pinglun',
+        color: '#3165aa'
+      },
+      {
+        text: '消息',
+        icon: 'icon-xiaoxi',
+        color: '#f7d615'
+      },
+    ],
+    listMenu: [
+      {
+        text: '心愿单',
+        icon: 'icon-shoucang1',
+        color: '#e94466',
+      },
+      {
+        text: '已购项目',
+        icon: 'icon-gouwuche',
+        color: '#e94466',
+      },
+      {
+        text: '设置',
+        icon: 'icon-shezhi1',
+        color: '#e94466',
+      },
+      {
+        text: '关于',
+        icon: 'icon-iconset0142',
+        color: '#e94466',
+      },
+    ]
   },
   methods: {
     jump() {
