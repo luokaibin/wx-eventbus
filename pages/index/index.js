19936e1ab4c8b260d2493c0ca6a8c3cd078b21af
胖子 <luokb@ebaiyihui.com>
2020-05-09 18:19:33 +0800

    update 策略模式

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,21 +3,25 @@ Component({
   data: {
     iconMenu: [
       {
+        id: 'prize',
         text: '奖品',
         icon: 'icon-libao',
         color: '#e94466',
       },
       {
+        id: 'gift',
         text: '礼包',
         icon: 'icon-gouwu',
         color: '#df0614'
       },
       {
+        id: 'comment',
         text: '评论',
         icon: 'icon-pinglun',
         color: '#3165aa'
       },
       {
+        id: 'msg',
         text: '消息',
         icon: 'icon-xiaoxi',
         color: '#f7d615'
@@ -25,21 +29,25 @@ Component({
     ],
     listMenu: [
       {
+        id: 'wish',
         text: '心愿单',
         icon: 'icon-shoucang1',
         color: '#e94466',
       },
       {
+        id: 'bought',
         text: '已购项目',
         icon: 'icon-gouwuche',
         color: '#e94466',
       },
       {
+        id: 'setup',
         text: '设置',
         icon: 'icon-shezhi1',
         color: '#e94466',
       },
       {
+        id: 'about',
         text: '关于',
         icon: 'icon-iconset0142',
         color: '#e94466',
@@ -47,10 +55,8 @@ Component({
     ]
   },
   methods: {
-    jump() {
-      wx.navigateTo({
-        url: '/pages/detail/index',
-      })
+    jump({currentTarget: {dataset: {id}}}) {
+      console.log(id)
     }
   }
 })
